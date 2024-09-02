3bfbbaa39267d8151284aea7523738e6ba144be8
CamiloRol <arodriguez@cesde.net>
2024-09-02 18:04:11 -0500

    version without dowhile

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 # agenciaXYZ
+=======
+README
+>>>>>>> 365c3e2 (Version without dowhile to repeat procedure)
---
+++ agencia.js
@@ -0,0 +1,180 @@
+//Clase constructora viaje
+
+//Variables
+let travels = [];
+let valueKid;
+let quanAdult;
+let quanKidos;
+let subTotal;
+let clientSelec;
+let inputGuajira;
+let inputCanon;
+let inputLlanos;
+let destiOptions;
+let total;
+let printQuote;
+
+let discount = 0;
+let nameCli;
+//Constantes
+
+const destinationsSpace = document.getElementById("destinationsSpace")
+const botonOptions = document.getElementById("botonOptions")
+const optionsSpace = document.getElementById("optionsSpace")
+const formSpace = document.getElementById("formSpace")
+const cotiCli = document.getElementById("cotiCli")
+const destinySelection = document.getElementById("destinySelection")
+const quoteSpace = document.getElementById("quoteSpace")
+
+//Clase que me guarda los destinos que tiene mi agencia
+class Travel {
+    constructor(name, valueAdul, photo, id) {
+        this.name = name
+        this.valueAdul = valueAdul
+        this.photo = photo
+        this.id = id
+    }
+}
+
+//Creacion de los destinos
+let guajira = new Travel("Guajira", 1980000, 'sources/guajiraPic.jpg', "guajira")
+let canonDelChicamocha = new Travel("Cañon Del Chicamocha", 1600000, 'sources/cañonPic.jpg', "canonDelChicamocha")
+let llanosOrientales = new Travel("Llanos Orientales", 1500000, 'sources/llanosPic.jpg', "llanosOrientales")
+
+//Guardar la informacion en la array
+travels.push(guajira,canonDelChicamocha,llanosOrientales)
+
+//Forma de precargar la informacion de la ventana
+window.addEventListener("load", pageHome)
+
+//Funciones
+function pageHome(){
+    //funcion que recorre el array y crea la tarjeta de cado mon
+    travels.forEach((Travel) => {
+        destiOptions = `
+        <input type="radio" name="mascota" id=${Travel.id} class="radio">
+        <label for=${Travel.id} >
+            <p>${Travel.name}</p>
+            <img src=${Travel.photo} alt=${Travel.photo}>
+        </label>
+        `
+        destinationsSpace.innerHTML += destiOptions
+
+        inputGuajira = document.getElementById('guajira')
+        inputCanon = document.getElementById('canonDelChicamocha')
+        inputLlanos = document.getElementById('llanosOrientales')
+    })
+
+    botonOptions.addEventListener('click', optionsSelection)
+    cotiCli.addEventListener('click', cotizar)
+    formSpace.style.display = "none"
+}
+
+function optionsSelection() {
+    optionsSpace.style.display = "none"
+    formSpace.style.display = "flex"
+    if (inputGuajira.checked) {
+        destinySelection.innerHTML = inputGuajira.id
+        clientSelec = inputGuajira.id
+    }else if(inputCanon.checked) {
+        destinySelection.innerHTML = inputCanon.id
+        clientSelec = inputCanon.id
+    }else if(inputLlanos.checked) {
+        destinySelection.innerHTML = inputLlanos.id
+        clientSelec = inputLlanos.id
+    }else {
+        alert('No has seleccionado un mon')
+        recargar()
+    }
+}
+
+function cotizar() {
+    subTotalProc()
+    discountProc()
+
+    total = subTotal - discount
+    formSpace.style.display = "none"
+    printQuotes()
+}
+
+function printQuotes() {
+    nameCli = document.getElementById("nameCli").value
+    if (clientSelec == "guajira") {
+        printQuote = `<div class="quote"><img src=${guajira.photo} alt=${guajira.photo}>
+        <pre>
+
+Nombre del destino: ${guajira.name}
+Nombre del cliente: ${nameCli}
+Valor por adulto: $ ${guajira.valueAdul}
+Valor por niño: $ ${valueKid}
+Nro Adultos: ${quanAdult}
+Nro Niños: ${quanKidos}
+Subtotal: $ ${subTotal}
+Vlr descuento: $ ${discount}
+Neto a pagar: $ ${total}
+            </pre></div>`
+
+        quoteSpace.innerHTML = printQuote 
+    }else if (clientSelec == "canonDelChicamocha") {
+        printQuote = `<div class="quote"><img src=${canonDelChicamocha.photo} alt=${canonDelChicamocha.photo}>
+        <pre>
+
+Nombre del destino: ${canonDelChicamocha.name}
+Nombre del cliente: ${nameCli}
+Valor por adulto: $ ${canonDelChicamocha.valueAdul}
+Valor por niño: $ ${valueKid}
+Nro Adultos: ${quanAdult}
+Nro Niños: ${quanKidos}
+Subtotal: $ ${subTotal}
+Vlr descuento: $ ${discount}
+Neto a pagar: $ ${total}
+            </pre></div>`
+
+        quoteSpace.innerHTML = printQuote 
+    }else if (clientSelec == "llanosOrientales") {
+        printQuote = `<div class="quote"><img src=${llanosOrientales.photo} alt=${llanosOrientales.photo}>
+        <pre>
+
+Nombre del destino: ${llanosOrientales.name}
+Nombre del cliente: ${nameCli}
+Valor por adulto: $ ${llanosOrientales.valueAdul}
+Valor por niño: $ ${valueKid}
+Nro Adultos: ${quanAdult}
+Nro Niños: ${quanKidos}
+Subtotal: $ ${subTotal}
+Vlr descuento: $ ${discount}
+Neto a pagar: $ ${total}
+            </pre></div>`
+
+        quoteSpace.innerHTML = printQuote 
+    }
+}
+
+function subTotalProc() {
+    quanAdult = document.getElementById("numAdul").value
+    quanKidos = document.getElementById("numKid").value
+
+    if (clientSelec == "guajira") {
+        valueKid = guajira.valueAdul * 0.6;
+        subTotal = (guajira.valueAdul * quanAdult) + (valueKid * quanKidos)
+    }else if (clientSelec == "canonDelChicamocha") {
+        valueKid = canonDelChicamocha.valueAdul * 0.6;
+        subTotal = (canonDelChicamocha.valueAdul * quanAdult) + (valueKid * quanKidos)
+    }else if (clientSelec == "llanosOrientales") {
+        valueKid = llanosOrientales.valueAdul * 0.6;
+        subTotal = (llanosOrientales.valueAdul * quanAdult) + (valueKid * quanKidos)
+    }
+
+}
+
+function discountProc() {
+
+    if (quanAdult > 4 && quanKidos > 2) {
+        discount = subTotal * 0.2
+    }
+}
+
+function recargar() {
+    location.reload()
+}
+
